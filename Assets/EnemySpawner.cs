ba6936fec8f7138d9ff36e6f7af680bc9c82a554
halfbro <mattlag@verizon.net>
2014-10-16 22:36:23 -0400

    Simple Enemy Spawner
    Simple Enemy Spawner

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawner : MonoBehaviour {
 
+    public List<EnemyShip> enemyList;
+    public ObjectPool BulletManager;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        InvokeRepeating("SpawnEnemy", 1.0f, 1.0f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void SpawnEnemy()
+    {
+        EnemyShip newEnemy = (EnemyShip)Instantiate(enemyList[0]);
+        newEnemy.BulletManager = BulletManager;
+        newEnemy.path = iTweenPath.paths["SmallFighter1"];
+    }
 }
